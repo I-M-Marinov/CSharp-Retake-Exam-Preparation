a498295acbee74eb3675804588761d1fd375110e
I-M-Marinov <lcfrrr@gmail.com>
2024-04-17 17:00:44 +0300

    Ex. 1 Structure // C# OOP Retake Exam – 16 April 2024
    Ex. 1 Structure
    C# OOP Retake Exam – 16 April 2024

---
+++ C# OOP Retake Exam – 16 April 2024/01.Structure/TheContentDepartment/Models/ContentMember.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TheContentDepartment.Utilities.Messages;
+
+namespace TheContentDepartment.Models
+{
+    public class ContentMember: TeamMember
+    {
+        private string[] validPaths = { "CSharp", "JavaScript", "Python", "Java" };
+        private string path;
+        public ContentMember(string name, string path) : base(name, path)
+        {
+            Path = path;
+        }
+
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                if (!validPaths.Any(p => p.ToLower() == value.ToLower()))
+                {
+                    throw new ArgumentException(ExceptionMessages.PathIncorrect, path);
+                }
+
+                path = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            int currentTasksCount = InProgress.Count;
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"{Name} - {Path} path. Currently working on {currentTasksCount} tasks.");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ C# OOP Retake Exam – 16 April 2024/01.Structure/TheContentDepartment/Models/TeamLead.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TheContentDepartment.Utilities.Messages;
+
+namespace TheContentDepartment.Models
+{
+    public class TeamLead:TeamMember
+    {
+        private string path;
+        public TeamLead(string name, string path) : base(name, path)
+        {
+            Path = path;
+        }
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                if (value != "Master")
+                {
+                    throw new ArgumentException(ExceptionMessages.PathIncorrect, path);
+                }
+
+                path = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            int currentTasksCount = InProgress.Count;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Name} ({GetType().Name}) - Currently working on {currentTasksCount} tasks.");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
